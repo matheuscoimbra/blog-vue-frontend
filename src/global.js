d3a1d573ee3d2da7836c8c3d314845e30f3f1501
mateus.morais <mateus.morais@a3solutions.com.br>
2019-08-15 15:15:15 -0300

    errors ok

--- src/global.js
+++ src/global.js
@@ -7,7 +7,7 @@ export const userKey = '__knowledge_user'
 export function showError(e) {
     console.log(e.response.data )
     if(e && e.response && e.response.data) {
-        Vue.toasted.global.defaultError({ msg : e.response.data.errors })
+        Vue.toasted.global.defaultError({ msg : e.response.data.errors[0] })
     } else if(typeof e === 'string') {
         Vue.toasted.global.defaultError({ msg : e })
     } else {
